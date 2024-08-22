fd71c794f59a46cd1b9c795ba99cd1b8efd34cdb
wkwooten <williamkwooten14@gmail.com>
2024-08-21 21:49:56 -0400

    Big Changes
    -added modal for mobile/tablet devices
    -fixed margin spacing

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-$(".toggle").hover(function () {
-  $(this).find(".toggleContainer").toggleClass("active");
-  $(this).sibling(".toggle").find(".toggleContainer").removeClass("active");
+// $(".toggle").hover(function () {
+//   $(this).find(".toggleContainer").toggleClass("active");
+//   $(this).sibling(".toggle").find(".toggleContainer").removeClass("active");
+// });
+
+console.log("test");
+
+$(".hamburger_menu").on("click", function () {
+  $(".modal").show();
+});
+
+$(".modal_menu").on("click", function () {
+  $(".modal").hide();
 });
