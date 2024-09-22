8ae60c325079ece8704d937463d4bd073e61a1d1
wkwooten <williamkwooten14@gmail.com>
2024-09-22 15:33:53 -0400

    Chevrons
    -Added indicator for homepage
    -started work on collaspable menus on case study page

--- js/testing.js
+++ js/testing.js
@@ -1,3 +1,19 @@
 u(".name").on('click', e => {
   u(".modal").show();
 });
+
+u(".indicator").on('click', e => {
+  u('.projects').scroll();
+});
+
+u(".chev").on('click', e => {
+  u('#overview').addClass('collapsed');
+  u('.chev').addClass('chev_rotate');
+});
+
+// u(".chev").on('click', e => {
+//   u('#overview').removeClass('collapsed');
+//   u('.chev').removeClass('chev_rotate');
+// });
+
+// if ("")
