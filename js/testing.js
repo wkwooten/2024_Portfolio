9ae352cbc191d8f0f296a8de04d89db757d8a151
wkwooten <williamkwooten14@gmail.com>
2024-09-23 21:57:20 -0400

    Accordian working!!!
    finally!

--- js/testing.js
+++ js/testing.js
@@ -11,18 +11,47 @@ u(".indicator").on('click', e => {
 //   u('.chev').addClass('chev_rotate');
 // });
 
-u(".modal_button").on('click', e => {
-  u('.modal').toggleClass('collapsed');
-  u('.chev').addClass('chev_rotate');
-});
+// u(".modal_button").on('click', e => {
+//   u('.modal').toggleClass('collapsed');
+//   u('.chev').addClass('chev_rotate');
+// });
 
 
-u(".chev").on('click', e => {
-  u('#overview').toggleClass('collapsed');
-  u('.chev').addClass('chev_rotate');
-});
+// u(".chev").on('click', e => {
+//   u('#overview').toggleClass('collapsed');
+//   u('.chev').addClass('chev_rotate');
+// });js/testing.js
 
 
 // el.classList.toggle(className);
 
 // chev.addEventListener('click', () => {});
+
+var acc = document.getElementsByClassName("section_header");
+var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+
+//     var cs_section = this.nextElementSibling;
+//     if (cs_section.style.display === "block") {
+//       cs_section.style.display = "none";
+//     } else {
+//       cs_section.style.display = "block";
+//     }
+//   });
+// }
+
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    u('.chev').toggleClass('chev_rotate')
+    var cs_section_content = this.nextElementSibling;
+    if (cs_section_content.style.maxHeight) {
+      cs_section_content.style.maxHeight = null;
+    } else {
+      cs_section_content.style.maxHeight = cs_section_content.scrollHeight + "px";
+    }
+  });
+}
