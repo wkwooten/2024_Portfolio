9965403e339a5dfce22d9bb1ee250fb02b2ba819
wkwooten <williamkwooten14@gmail.com>
2024-09-27 22:57:50 -0400

    logo + intro rev

--- js/testing.js
+++ js/testing.js
@@ -18,30 +18,9 @@ u(".indicator").on('click', e => {
   u('.projects').scroll();
 });
 
-// Select all section headers (accordions)
-// var acc = document.querySelectorAll('.section_header');
-
-// // Loop through each header
-// for (let i = 0; i < acc.length; i++) {
-//   // Set initial state to expanded
-//   let content = acc[i].nextElementSibling;
-//   content.style.maxHeight = content.scrollHeight + "px";
-
-//   // Add click event to each header
-//   acc[i].addEventListener('click', function() {
-//     let content = this.nextElementSibling;
-
-//     // Toggle the rotation of the arrow (chevron)
-//     this.querySelector('.chev').classList.toggle('chev_rotate');
-
-//     // Toggle between expanded and collapsed states
-//     if (content.style.maxHeight && content.style.maxHeight !== "0px") {
-//       content.style.maxHeight = "0px"; // Collapse
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px"; // Expand
-//     }
-//   });
-// }
+u(".name").on('click', e => {
+  u('name').addClass('bounce');
+});
 
 var acc = document.querySelectorAll('.section_header');
 
