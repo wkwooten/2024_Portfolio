209a7be444c74249a0eec0350d0112caddbe9036
wkwooten <williamkwooten14@gmail.com>
2025-03-11 18:37:17 -0400

    Update menu.js
    for icon consistency

--- js/menu.js
+++ js/menu.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
       // 1. Initialize (hamburger menu)
       hamburger: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
           <title>Open Menu</title>
-          <g fill="none" stroke="currentColor" stroke-dasharray="16" stroke-dashoffset="16" stroke-linecap="none" stroke-linejoin="round" stroke-width="2">
+          <g fill="none" stroke="currentColor" stroke-dasharray="16" stroke-dashoffset="16" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
             <path d="M5 5h14">
               <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="16;0" />
             </path>
